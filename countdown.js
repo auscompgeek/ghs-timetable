fe141bf79d028e9a14901dc7b54434a6b02e3adc
David <auscompgeek@users.noreply.github.com>
2014-06-24 23:22:47 +1000

    countdown.js: events start at seconds=0

--- countdown.js
+++ countdown.js
@@ -40,6 +40,7 @@ function getSecondsUntilEvent(ev) {
 	var evDate = new Date();
 	evDate.setHours(ev.hour);
 	evDate.setMinutes(ev.minute);
+	evDate.setSeconds(0);
 	return ((new Date() - evDate)/1000)|0;
 }
 
