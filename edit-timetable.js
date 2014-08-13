e4d97fec07d5351a88fa00314916f7c0fea60fd1
David <auscompgeek@users.noreply.github.com>
2014-08-13 23:09:53 +1000

    edit-timetable.js: nicer do(Im|Ex)port()
    * doImport() now tells the user what's happened.
    * doExport() can now export half-data.

--- edit-timetable.js
+++ edit-timetable.js
@@ -33,18 +33,28 @@ function toggleUseTimetable() {
 }
 
 function doImport() {
-	var obj = JSON.parse($("#text-import").text());
-	if (obj.classes) {
-		localStorage.classes = JSON.stringify(this.text);
-	}
-	if (obj.days) {
-		localStorage.days = JSON.stringify(this.text);
+	var text = document.getElementById("text-import").value;
+	var obj;
+	try {
+		obj = JSON.parse(text);
+	} catch (ex) {}
+
+	if (obj && (obj.classes || obj.days)) {
+		if (obj.classes) {
+			localStorage.classes = JSON.stringify(obj.classes);
+		}
+		if (obj.days) {
+			localStorage.days = JSON.stringify(obj.days);
+		}
+		alert("Done!");
+	} else {
+		alert("That doesn't look like timetable data!");
 	}
 }
 
 function doExport() {
 	$("#text-export").text(JSON.stringify({
-		classes: JSON.parse(localStorage.classes),
-		days: JSON.parse(localStorage.days)
+		classes: localStorage.classes && JSON.parse(localStorage.classes),
+		days: localStorage.days && JSON.parse(localStorage.days)
 	}));
 }
